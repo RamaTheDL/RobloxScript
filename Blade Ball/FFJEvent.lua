c66fb94846eaa9dda6a98f4c09794d220f5bee4d
Rama <121490151+RamaTheDL@users.noreply.github.com>
2024-06-09 06:43:46 +0700

    Update FFJEvent.lua

--- Blade Ball/FFJEvent.lua
+++ Blade Ball/FFJEvent.lua
@@ -2,5 +2,5 @@ local queueonteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or 
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/FFJ1/Roblox-Exploits/main/scripts/BladeBallEvent.lua'))()
 if queueonteleport then
-    queueonteleport("https://raw.githubusercontent.com/RamaTheDL/RobloxScript/main/Blade%20Ball/FFJEvent.lua")
+    queueonteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/RamaTheDL/RobloxScript/main/Blade%20Ball/FFJEvent.lua'))()")
 end
